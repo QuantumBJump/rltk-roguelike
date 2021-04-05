969be07351184211d40f1df06cafb2339957cc73
Quinn Stevens <quantumjump451@gmail.com>
2021-04-05 16:01:09 +0100

    Factored out spawning

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ use damage_system::DamageSystem;
 mod gui;
 mod gamelog;
 pub use gamelog::GameLog;
+mod spawner;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum RunState { AwaitingInput, PreRun, PlayerTurn, MonsterTurn }
@@ -126,63 +127,23 @@ fn main() -> rltk::BError {
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
 
-    // Create a monster at the center of each room (except the first, the player's there)
-    let mut rng = rltk::RandomNumberGenerator::new();
-    for (i, room) in map.rooms.iter().skip(1).enumerate() {
-        let (x, y) = room.center();
-
-        let glyph: rltk::FontCharType;
-        let name: String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
-            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
-        }
-        gs.ecs.create_entity()
-            .with(Position{ x, y })
-            .with(Renderable{
-                glyph: glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed { visible_tiles: Vec::new(), range: 8, dirty: true })
-            .with(Monster {})
-            .with(Name{ name: format!("{} #{}", &name, i)})
-            .with(BlocksTile {})
-            .with(CombatStats{
-                max_hp: 16,
-                hp: 16,
-                defense: 1,
-                power: 4,
-            })
-            .build();
-    }
-
     // Create player entity
-    let player_entity = gs.ecs
-        .create_entity()
-        .with(Position {x: player_x, y: player_y})
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player {})
-        .with(Viewshed{ visible_tiles: Vec::new(), range: 8 , dirty: true})
-        .with(Name{name: "Player".to_string()})
-        .with(CombatStats{
-            max_hp: 30,
-            hp: 30,
-            defense: 2,
-            power: 5,
-        })
-        .build();
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
+
+    // Seed rng
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    // Create a monster at the center of each room (except the first, the player's there)
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        spawner::random_monster(&mut gs.ecs, x, y);
+    }
 
     gs.ecs.insert(map);
     gs.ecs.insert(player_entity);
     gs.ecs.insert(Point::new(player_x, player_y));
     gs.ecs.insert(RunState::PreRun);
     gs.ecs.insert(gamelog::GameLog{ entries: vec!["Welcome to Rustlike!".to_string()]});
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
 
     rltk::main_loop(context, gs)
 }
