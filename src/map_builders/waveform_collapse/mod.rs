b059adf481415eb30a176d4ab29e07dde62cf9c5
Quinn Stevens <quantumjump451@gmail.com>
2021-04-26 16:17:25 +0100

    Building pattern gallery

--- src/map_builders/waveform_collapse/mod.rs
+++ src/map_builders/waveform_collapse/mod.rs
@@ -8,6 +8,8 @@ use specs::prelude::*;
 
 mod image_loader;
 use image_loader::*;
+mod constraints;
+use constraints::*;
 
 pub struct WaveformCollapseBuilder {
     map: Map,
@@ -65,23 +67,55 @@ impl WaveformCollapseBuilder {
     fn build(&mut self) {
         let mut rng = RandomNumberGenerator::new();
 
-        self.map = load_rex_map(self.depth, &rltk::rex::XpFile::from_resource("../resources/wfc-demo1.xp").unwrap());
+        // Size of a chunk
+        const CHUNK_SIZE: i32 = 7;
+
+        self.map = load_rex_map(self.depth, &rltk::rex::XpFile::from_resource("../resources/wfc-demo2.xp").unwrap());
         self.take_snapshot();
+
+        let patterns = build_patterns(&self.map, CHUNK_SIZE, true, true);
+        self.render_tile_gallery(&patterns, CHUNK_SIZE);
 
         // Find a starting point; start at the middle & walk left until we find an open space
         self.starting_position = Position{ x: self.map.width / 2, y: self.map.height / 2 };
         let start_idx = self.map.xy_idx(self.starting_position.x, self.starting_position.y);
-        self.take_snapshot();
 
         // Find all tiles we can reach from the starting point
         let exit_tile = get_most_distant_area(&mut self.map, start_idx, true);
-        self.take_snapshot();
 
         // Place stairs
         self.map.tiles[exit_tile] = TileType::DownStairs;
-        self.take_snapshot();
 
         // Build a noise map
         self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
+    }
+
+    fn render_tile_gallery(&mut self, patterns: &Vec<Vec<TileType>>, chunk_size: i32) {
+        self.map = Map::new(0);
+        let mut counter = 0;
+        let mut x = 1;
+        let mut y = 1;
+        while counter < patterns.len() {
+            render_pattern_to_map(&mut self.map, &patterns[counter], chunk_size, x, y);
+
+            x += chunk_size +1;
+            if x + chunk_size > self.map.width {
+                // Move to next row
+                x = 1;
+                y += chunk_size + 1;
+
+                if y + chunk_size > self.map.height {
+                    // Move to next page
+                    self.take_snapshot();
+                    self.map = Map::new(0);
+
+                    x = 1;
+                    y = 1;
+                }
+            }
+
+            counter += 1;
+        }
+        self.take_snapshot();
     }
 }
