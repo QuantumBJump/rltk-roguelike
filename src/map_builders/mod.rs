975790ecebd740641ba0756f7a9301f51bbbae12
Quinn Stevens <quantumjump451@gmail.com>
2021-05-06 13:22:18 +0100

    Updated WFC

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -158,7 +158,8 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32, rng: &mut rltk::RandomNumberGenerator) -> BuilderChain {
     let mut builder = BuilderChain::new(new_depth);
-    builder.start_with(VoronoiCellBuilder::new());
+    builder.start_with(VoronoiCellBuilder::pythagoras());
+    builder.with(WaveformCollapseBuilder::new());
     builder.with(AreaStartingPosition::new(XStart::CENTER, YStart::CENTER));
     builder.with(CullUnreachable::new());
     builder.with(VoronoiSpawning::new());
